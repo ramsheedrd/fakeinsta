74a74bf958fc0f3dbb122b87dc0b742be6a8163a
ramsheedrd <ramsheedkc10@gmail.com>
2021-02-10 13:53:08 +0530

    ajax 'like' feature & 'comment' feature added

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("add/", views.add_post, name="add_post"),
-    path('like/<post_id>/', views.like_toggle)
+    path('like/<post_id>/', views.like_toggle),
+    path('comment/add/', views.add_comment)
     ]
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import PostAddForm
-from .models import Post, Like
+from .models import Comment, Like, Post
 
 # Create your views here.
 
 
 @login_required
 def home_view(request):
-    posts = Post.objects.all().prefetch_related("post_likes").select_related("user")
+    posts = Post.objects.all().prefetch_related("post_likes", 'post_comments', 'post_comments__user').select_related("user")
     likes = Like.objects.filter(post__in=posts, user=request.user).values_list("post", flat=True)
 
     def is_liked_add(p):
@@ -42,11 +44,28 @@ def add_post(request):
 def like_toggle(request, post_id):
     post = Post.objects.get(id=post_id)
     user = request.user
-
+    response = {}
     try:
         already_liked = Like.objects.get(post=post, user=user)
         already_liked.delete()
+        response['status'] = 'dislike'
     except Like.DoesNotExist:
         Like.objects.create(post=post, user=user)
+        response['status'] = 'like'
 
-    return redirect("posts:home")
+    response['count'] = Like.objects.filter(post=post).count()
+    return JsonResponse(response)
+
+
+@csrf_exempt
+def add_comment(request):
+    post_id = request.POST.get('post_id')
+    comment = request.POST.get('comment')
+
+    post = Post.objects.get(id=post_id)
+    user = request.user
+
+    Comment.objects.create(user=user, post=post, comment=comment)
+    return JsonResponse({'user':user.first_name, 'comment':comment})
+
+
